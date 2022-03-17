882f36e019e1e0dcdba96d6f3e4ac277227afe7b
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-03-17 19:28:54 +1000

    added functions for student, added Mentor class

--- Projects/Class/OOP.py
+++ Projects/Class/OOP.py
@@ -15,3 +15,38 @@ class Student:
                 lecturer.eval[course] = [grade]
         else:
             return 'Ошибка'
+
+    def average_grade(self):
+        total = 0
+        length = 0
+        for value in self.grades.values():
+            total += sum(value)
+            length += len(value)
+        return round(float(total / length), 2)
+
+
+    def __str__(self):
+        text = f"Имя: {self.name} " \
+               f"\nФамилия: {self.surname} " \
+               f"\nСредняя оценка за домашние задания: {self.average_grade()} " \
+               f"\nКурсы в процессе изучения: {', '.join(self.courses_in_progress)}" \
+               f"\nЗавершенные курсы: {', '.join(self.finished_courses)}"
+        return text
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() < other.average_grade()
+        else:
+            return 'Error'
+
+    def __gt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() > other.average_grade()
+        else:
+            return 'Error'
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
