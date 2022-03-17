23bb89ea72ed0f0b0cdf8c218aa3bffafb7a2282
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-03-17 19:40:48 +1000

    added objects and arguments for Classes

--- Projects/Class/OOP.py
+++ Projects/Class/OOP.py
@@ -94,3 +94,74 @@ class Lecturer(Mentor):
             return self.average_eval() > other.average_eval()
         else:
             return 'Error'
+
+student_1 = Student('Ruoy', 'Eman', 'male')
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
+student_1.finished_courses += ['Введение в программирование']
+
+first_reviewer = Reviewer('Some', 'Buddy')
+first_reviewer.courses_attached += ['Python']
+first_reviewer.courses_attached += ['Введение в программирование']
+
+student_2 = Student('John', 'Snow', 'male')
+student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
+student_2.finished_courses += ['Введение в программирование']
+
+first_reviewer.rate_hw(student_1, 'Введение в программирование', 7)
+first_reviewer.rate_hw(student_1, 'Введение в программирование', 10)
+first_reviewer.rate_hw(student_1, 'Введение в программирование', 8)
+
+first_reviewer.rate_hw(student_2, 'Введение в программирование', 5)
+first_reviewer.rate_hw(student_2, 'Введение в программирование', 9)
+first_reviewer.rate_hw(student_2, 'Введение в программирование', 9)
+
+first_reviewer.rate_hw(student_1, 'Python', 7)
+first_reviewer.rate_hw(student_1, 'Python', 10)
+first_reviewer.rate_hw(student_1, 'Python', 10)
+first_reviewer.rate_hw(student_1, 'Python', 7)
+
+second_reviewer = Reviewer('New', 'Man')
+second_reviewer.courses_attached += ['Python']
+second_reviewer.courses_attached += ['Git']
+
+first_lecturer = Lecturer('Ned', 'Stark')
+first_lecturer.courses_attached += ['Python']
+first_lecturer.courses_attached += ['Git']
+
+student_2.rate_lecture(first_lecturer, 'Python', 8)
+student_2.rate_lecture(first_lecturer, 'Python', 5)
+student_2.rate_lecture(first_lecturer, 'Python', 5)
+student_2.rate_lecture(first_lecturer, 'Python', 8)
+
+second_lecturer = Lecturer('Sirius', 'Snape')
+second_lecturer.courses_attached += ['Python']
+second_lecturer.courses_attached += ['Введение в программирование']
+
+student_1.rate_lecture(second_lecturer, 'Python', 7)
+student_1.rate_lecture(second_lecturer, 'Python', 6)
+student_1.rate_lecture(second_lecturer, 'Python', 9)
+student_2.rate_lecture(first_lecturer, 'Python', 8)
+
+second_reviewer.rate_hw(student_2, 'Python', 9)
+second_reviewer.rate_hw(student_2, 'Python', 10)
+second_reviewer.rate_hw(student_2, 'Python', 10)
+second_reviewer.rate_hw(student_2, 'Python', 10)
+
+
+print(student_1.grades)
+print(student_2.grades)
+print(first_lecturer.eval)
+print(second_lecturer.eval)
+print(first_reviewer)
+print(second_reviewer)
+print(first_lecturer)
+print(second_lecturer)
+print(student_1)
+print(student_2)
+print(student_1 < student_2)
+print(first_lecturer > second_lecturer)
+
+students_list = [student_1, student_2]
+lecturers_list = [first_lecturer, second_lecturer]
