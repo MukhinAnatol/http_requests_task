2d4360c5453eba16557c3b08f213cac263660c42
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-03-17 19:39:22 +1000

    added Reviewer and Lecturer class

--- Projects/Class/OOP.py
+++ Projects/Class/OOP.py
@@ -50,3 +50,47 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+class Reviewer(Mentor):
+
+    def rate_hw (self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress or course in student.finished_courses:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        text = f"Имя: {self.name} \nФамилия: {self.surname}"
+        return text
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.eval = {}
+
+    def average_eval(self):
+        total = 0
+        length = 0
+        for value in self.eval.values():
+            total += sum(value)
+            length += len(value)
+        return round(float(total / length), 2)
+
+    def __str__(self):
+            text = f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.average_eval()}"
+            return text
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_eval() < other.average_eval()
+        else:
+            return 'Error'
+
+    def __gt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_eval() > other.average_eval()
+        else:
+            return 'Error'
