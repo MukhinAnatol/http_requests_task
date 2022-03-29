f17f71068659e2e9555084cf406a87344e5bd10f
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-03-29 20:07:52 +1000

    examples deleted, final commit

--- Projects/python_study/readfile.py
+++ Projects/python_study/readfile.py
@@ -16,25 +16,6 @@ def get_recipes(file_name: str):
 
 print(get_recipes('recipes.txt'))
 
-example_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
-
 def get_shop_list_by_dishes(dishes: list, person_count: int):
     cook_book = get_recipes('recipes.txt')
     shopping_list = {}
@@ -50,12 +31,3 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
     return shopping_list
 
 print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 4))
-
-example_shopping = {
-  'Картофель': {'measure': 'кг', 'quantity': 2},
-  'Молоко': {'measure': 'мл', 'quantity': 200},
-  'Помидор': {'measure': 'шт', 'quantity': 4},
-  'Сыр гауда': {'measure': 'г', 'quantity': 200},
-  'Яйцо': {'measure': 'шт', 'quantity': 4},
-  'Чеснок': {'measure': 'зубч', 'quantity': 6}
-}
